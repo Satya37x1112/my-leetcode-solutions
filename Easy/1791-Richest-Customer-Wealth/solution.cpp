13ca56716b299226e5dff9575fa6b13eab6852e4
Satya Sarthak Manohari <121684661+Satya37x1112@users.noreply.github.com>
2026-07-27 14:10:38 +0530

    Solved 1791 - Richest Customer Wealth

---
+++ Easy/1791-Richest-Customer-Wealth/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int rich = INT_MIN;
+        for(int i = 0; i < accounts.size(); i++)
+        {
+            int trich = 0;
+            for(int j = 0; j < accounts[i].size(); j++)
+            {
+                trich += accounts[i][j];
+            }
+            if(trich > rich)
+            {
+                rich = trich;
+            }
+        }
+        return rich;
+    }
+};
