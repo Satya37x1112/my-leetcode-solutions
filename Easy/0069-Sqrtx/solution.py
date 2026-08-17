587e5897fda3bf7ac963577680af4017dbc5c573
Satya Sarthak Manohari <121684661+Satya37x1112@users.noreply.github.com>
2026-08-17 11:54:11 +0530

    Solved 0069 - Sqrt(x)

---
+++ Easy/0069-Sqrtx/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    import math
+    def mySqrt(self, x):
+        p=int(math.sqrt(x))
+        return p
+        """
+        :type x: int
+        :rtype: int
+        """
+        
